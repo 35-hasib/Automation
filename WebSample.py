9a0c0c974680138da63d7182f7d9e9f7e60ed789
35-hasib <hrrahman35@gmail.com>
2024-01-27 17:23:06 +0600

    First commit

---
+++ WebSample.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import pandas as pd
+
+driver = webdriver.Chrome()
+driver.get('https://www.thesun.co.uk/tech/')
+contents = driver.find_elements(by='xpath', value='//div[@class="teaser__copy-container"]')
+
+titles = []
+subtitles = []
+links = []
+
+for content in contents:
+    title = content.find_element(by='xpath', value='//div[@class="teaser__copy-container"]/a/h3').text
+    subtitle = content.find_element(by='xpath', value='//div[@class="teaser__copy-container"]/a/p').text
+    link = content.find_element(by='xpath', value='//div[@class="teaser__copy-container"]/a').get_attribute("href")
+
+    titles.append(title)
+    subtitles.append(subtitle)
+    links.append(link)
+
+my_disk = {'titles':titles,'subtitles':subtitles,'links':links,}
+
+allData = pd.DataFrame(my_disk)
+allData.to_markdown('allData.md')
+
+driver.quit()
