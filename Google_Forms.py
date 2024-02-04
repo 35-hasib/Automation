0b3aebed4c3ebd21f1b162e27c73126a9043e73c
35-hasib <hrrahman35@gmail.com>
2024-02-04 23:40:53 +0600

    :

--- Google_Forms.py
+++ Google_Forms.py
@@ -6,7 +6,7 @@ options.add_argument('headless')
 driver = webdriver.Chrome(options=options)
 
 driver.get('https://forms.gle/dHdyrY6tb3JDfsPPA')
-driver.find_element(by='xpath', value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys('Hasibur Rahman')
+driver.find_element(by='xpath', value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys('Md. Hasibur Rahman')
 driver.find_element(by='xpath', value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys('2010977137')
 driver.find_element(by='xpath', value='//div[@role="button"]').click()
 
